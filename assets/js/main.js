d542cd2369e3de2986669d350bce4f45c229253a
Ella Choy <egal21de@gmail.com>
2021-07-15 15:45:58 +0200

    js console.log

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+let a=12;
+console.log(a)
+let b=a*3;
+console.log(b);
+
+
+// document.getElementById("result").textContent = b;
+console.log(document.URL);
+
+console.log("Hello world!");
+
+
+console.log("Anton");
+
+let name = "Anton";
+
+console.log(name);
+
+
+let age = 28;
+
+console.log(age);
+
+
+let job = "Trainer";
+
+console.log(job);
+
+var married = true;
+
+console.log(married);
+
+
+
+console.log(name + " ist " + age + " und ist verheiratet: " + married);
